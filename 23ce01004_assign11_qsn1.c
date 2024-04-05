a054875a6ce81cbcdc43d75fd196c71ce26d5703
andalibhasan <157709927+andalibhasan@users.noreply.github.com>
2024-04-05 14:30:39 +0530

    23ce01004_assign11_qsn1.c

--- 23ce01004_assign11_qsn1.c
+++ 23ce01004_assign11_qsn1.c
@@ -1,28 +1,18 @@
-#include <stdio.h>
-#include <string.h>
-
-#define MAX_SIZE 100
-
-struct Stack {
-    int top;
-    char items[MAX_SIZE];
-};
-
-void push(struct Stack *s, char c) { s->items[++(s->top)] = c; }
-char pop(struct Stack *s) { return s->items[(s->top)--]; }
-
-void reverseString(char *str) {
-    struct Stack s = { .top = -1 };
-    for (int i = 0; str[i] != '\0'; i++) push(&s, str[i]);
-    for (int i = 0; s.top != -1; i++) str[i] = pop(&s);
-}
+#include<stdio.h>
+#include<string.h>
 
 int main() {
-    char str[MAX_SIZE];
-    printf("Input a string: ");
-    fgets(str, MAX_SIZE, stdin);
-    if (str[strlen(str) - 1] == '\n') str[strlen(str) - 1] = '\0';
-    reverseString(str);
-    printf("Reversed string using a stack is: %s\n", str);
+    char arr[100];
+    int top=-1;
+
+    printf("Enter a string:");
+    gets(arr);
+
+    top=strlen(arr)-1;
+
+    for(int i=top;i>=0;i--)              //reversing
+    {
+        printf("%c",arr[i]);
+    }
     return 0;
 }
